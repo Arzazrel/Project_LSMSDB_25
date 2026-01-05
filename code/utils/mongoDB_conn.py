8b0f208b08a0f730c990878d366e00554c799e36
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2026-01-05 17:55:21 +0100

    Update mongoDB_conn.py
    add funcionality for big injestion of data

--- code/utils/mongoDB_conn.py
+++ code/utils/mongoDB_conn.py
@@ -16,16 +16,30 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
 # -- DB config parameters --
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "myfuture_db"
+#MONGO_URI = "mongodb://localhost:27017"     # case of no replica set
+# case of replica set, ensure tu connect to primary replica
+MONGO_URI = (
+    "mongodb://localhost:27017,localhost:27018,localhost:27019/"
+    "?replicaSet=rs0"
+)
+DB_NAME = "myfuture_lsmsdb_2025"
 
-# return the connection to myfuture_db or None in case of error
-def get_db():
+# return the connection to myfuture_lsmsdb_2025 or None in case of error
+def get_db(majority=False):
     try:
-        client = MongoClient(
-            MONGO_URI,
-            serverSelectionTimeoutMS=3000
-        )
+        if majority:
+            client = MongoClient(
+                MONGO_URI,
+                serverSelectionTimeoutMS=3000,
+                w=majority
+            )
+        else:
+            client = MongoClient(
+                MONGO_URI,
+                serverSelectionTimeoutMS=4000,
+                w=1,                                # must wait for ack only from the primary node
+                retryWrites=False                   # avoid retry during step-down
+            )
         client.admin.command("ping")        # check with ping
         return client[DB_NAME]              # return the DB object
 
