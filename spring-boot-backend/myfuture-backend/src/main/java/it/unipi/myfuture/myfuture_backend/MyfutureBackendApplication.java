971ce54aee9e696ffed37fbec41a86bbf24405f4
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2026-01-12 10:58:41 +0100

    added backend project

---
+++ spring-boot-backend/myfuture-backend/src/main/java/it/unipi/myfuture/myfuture_backend/MyfutureBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.myfuture.myfuture_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfutureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfutureBackendApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend/myfuture-backend/src/test/java/it/unipi/myfuture/myfuture_backend/MyfutureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.myfuture.myfuture_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfutureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
